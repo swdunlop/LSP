38bfa75480354f2f203ecbdb154e61eb7e879343
Scott W. Dunlop <swdunlop@gmail.com>
2017-09-13 09:49:03 -0700

    LspCodeActionsCommand.is_enabled must return True or False
    Sublime Text 3 requires TextCommand.is_enabled methods return either True or False; if
    client_for_view returns None, or another false-ish value, LspCodeActionsCommand.is_enabled
    must return False.

--- main.py
+++ main.py
@@ -1929,7 +1929,8 @@ class LspCodeActionsCommand(sublime_plugin.TextCommand):
     def is_enabled(self, event=None):
         if is_supported_view(self.view):
             client = client_for_view(self.view)
-            return client and client.has_capability('codeActionProvider')
+            if client and client.has_capability('codeActionProvider'):
+            	return True
         return False
 
     def run(self, edit, event=None):
